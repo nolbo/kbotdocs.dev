dbbc17d7d2a8f3195d12800e500d49f7fe966124
nolbo <jun9975sun@gmail.com>
2024-09-14 16:37:40 +0900

    robot.txt, sitemap 추가

--- app/robots.ts
+++ app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
             userAgent: "*",
             allow: "/"
         },
-        sitemap: "https://nolbo.dev/sitemap.xml"
+        sitemap: "https://kbotdocs.dev/sitemap.xml"
     };
 }
--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,12 +3,28 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
-            url: "https://nolbo.dev",
+            url: "https://kbotdocs.dev",
             lastModified: new Date()
         },
         {
-            url: "https://nolbo.dev/post/11",
+            url: "https://kbotdocs.dev/summary",
             lastModified: new Date()
-        }
+        },
+        {
+            url: "https://kbotdocs.dev/legacy",
+            lastModified: new Date()
+        },
+        {
+            url: "https://kbotdocs.dev/api2",
+            lastModified: new Date()
+        },
+        {
+            url: "https://kbotdocs.dev/starlight",
+            lastModified: new Date()
+        },
+        {
+            url: "https://kbotdocs.dev/advanced",
+            lastModified: new Date()
+        },
     ];
 }
