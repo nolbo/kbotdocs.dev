47a645c530d9f60964f005e797c11d21f035033b
nolbo <jun9975sun@gmail.com>
2025-02-22 19:46:49 +0900

    Feature: Add Divider in PageTree

--- components/common/PageTree/index.tsx
+++ components/common/PageTree/index.tsx
@@ -9,14 +9,26 @@ interface IPageTree extends HTMLAttributes<HTMLElement> {
 
 export default function PageTree({ className, onLinkClick, ...p }: IPageTree) {
     return (
-        <nav className={`h-full overflow-auto ${className || ""}`} {...p}>
+        <nav className={`flex flex-col gap-[4px] h-full overflow-auto ${className || ""}`} {...p}>
             {
-                Docs.map((e: Doc, i: number) => {
-                    if (e.children && e.children.length > 0) {
-                        return (<PageDir key={i} doc={e} onLinkClick={ onLinkClick } />);
-                    }
-                    return (<Page key={i} doc={e} onLinkClick={ onLinkClick } />);
-                })
+                Object.values(Docs).filter(e => !e.hide).map(e => (
+                    <div key={e.label}>
+                        <div className={"flex items-center gap-[8px] mb-[4px]"}>
+                            <p className={"text-sm text-description"}>{e.label}</p>
+                            <div className={"flex-1 h-[1px] bg-[theme(borderColor.default)]"}></div>
+                        </div>
+                        <div>
+                            {
+                                e.docs.map((doc: Doc, i: number) => {
+                                    if (doc.children && doc.children.length > 0) {
+                                        return (<PageDir key={i} doc={doc} onLinkClick={ onLinkClick } />);
+                                    }
+                                    return (<Page key={i} doc={doc} onLinkClick={ onLinkClick } />);
+                                })
+                            }
+                        </div>
+                    </div>
+                ))
             }
         </nav>
     );
--- types/docs.d.ts
+++ types/docs.d.ts
@@ -1,5 +1,22 @@
-type Doc = {
+interface Doc {
+    label: string;
+    path?: string;
+    children?: (Doc | Article)[];
+}
+
+interface Article extends Doc {
+    author: Author | Author[];
+    description: string;
+    thumbnail: string;
+    publishDate: Date;
+}
+
+type DocCategory = {
     label: string,
-    path?: string,
-    children?: Doc[],
-};
+    hide?: boolean,
+    docs: (Doc | Article)[]
+}
+
+type Author = {
+    name: string
+}
